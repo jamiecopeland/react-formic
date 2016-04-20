5c0af42c4745e7408af7eeca5c206410ab2597f9
Jamie Copeland <jamie@jamiecopeland.com>
2016-04-20 18:26:29 +0100

    Tidy formalizeWrapper

--- src/components/formalizeWrapper.js
+++ src/components/formalizeWrapper.js
@@ -4,7 +4,6 @@ import { Subject } from 'rx';
 
 import { INVALID } from '../constants/validationStates';
 import { mapObjectToObject } from 'formalizer/lib/utils/objectUtils';
-import { logImmutable } from '../utils/immutableUtils';
 
 import {
   Field,
@@ -18,7 +17,92 @@ function cleanValidationOutput({ validity, validityMessage }) {
   };
 }
 
-export function formalize(config, mapFormToProps) {
+function createTriggerFieldMap(fields) {
+  return Object.keys(fields)
+    .filter(fieldName => fields[fieldName].triggerFields)
+    .reduce((acc, fieldName) => {
+      fields[fieldName].triggerFields.forEach(triggerFieldName => {
+        acc[triggerFieldName].push(fieldName);
+      });
+      return acc;
+    }, mapObjectToObject(fields, () => []));
+}
+
+function createFieldValidators(configFields, getFormState, setFormField) {
+  return mapObjectToObject(configFields, (field, fieldName) => {
+    let output;
+
+    if (field.validate) {
+      const subject = new Subject();
+      const stream = field.validate(subject, getFormState).subscribe(
+        value => {
+          // console.log('validation', value);
+          setFormField({
+            field: Map(cleanValidationOutput(value)),
+            fieldName,
+          });
+        }
+      );
+      output = { stream, subject };
+    } else {
+      output = null;
+    }
+
+    return output;
+  });
+}
+
+function triggerRelatedFields(relatedFields, fieldValidators, getFormState, setFormField) {
+  // Trigger other fields that need to know when this one changes
+  relatedFields.forEach(fieldNameToTrigger => {
+    // Set the field to dirty if it isn't already
+    const fieldToTrigger = getFormState().getIn(['fields', fieldNameToTrigger]);
+    if (!fieldNameToTrigger.isDirty) {
+      setFormField({ fieldName: fieldNameToTrigger, field: Map({ isDirty: true }) });
+    }
+
+    // Trigger the field to revalidate with its current value
+    fieldValidators[fieldNameToTrigger].subject.onNext(fieldToTrigger.value);
+  });
+}
+
+function createFieldChangeHandlers(
+  configFields, triggerFieldMap, fieldValidators, getFormState, setFormField
+) {
+  return mapObjectToObject(configFields, (field, fieldName) => value => {
+    setFormField({
+      fieldName,
+      field: Map({
+        isDirty: true,
+        value: field.transform ? field.transform(value) : value,
+      }),
+    });
+
+    triggerRelatedFields(triggerFieldMap[fieldName], fieldValidators, getFormState, setFormField);
+  });
+}
+
+function createEmptyForm(fields) {
+  return new Form({
+    fields: Map(mapObjectToObject(fields, field => new Field({
+      value: field.initialValues ? field.initialValues.value : undefined,
+    }))),
+  });
+}
+
+function defaultMapFormToProps(formState) {
+  return { form: formState };
+}
+
+function getFieldsWithDiff(formState1, formState2) {
+  return formState2.fields.reduce((acc, field, fieldName) => {
+    return !formState1 || field !== formState1.fields.get(fieldName)
+      ? acc.set(fieldName, field)
+      : acc;
+  }, Map({}));
+}
+
+export function formalize(config, mapFormToProps = defaultMapFormToProps) {
   return ComponentToWrap => {
     class FormalizeComponent extends React.Component {
 
@@ -44,79 +128,25 @@ export function formalize(config, mapFormToProps) {
       }
 
       componentWillMount() {
-        this.triggerFieldMap = Object.keys(config.fields)
-        .filter(fieldName => config.fields[fieldName].triggerFields)
-        .reduce((acc, fieldName) => {
-          config.fields[fieldName].triggerFields.forEach(triggerFieldName => {
-            acc[triggerFieldName].push(fieldName);
-          });
-          return acc;
-        }, mapObjectToObject(config.fields, () => []));
+        const { formState, setFormField, initializeForm } = this.props;
+        const { fields, name } = config;
 
-        // Create validation streams
-        this.fieldValidators = mapObjectToObject(config.fields, (field, fieldName) => {
-          let output;
+        this.triggerFieldMap = createTriggerFieldMap(fields);
+        this.fieldValidators = createFieldValidators(fields, this.getFormState, setFormField);
+        this.fieldChangeHandlers = createFieldChangeHandlers(
+          fields, this.triggerFieldMap, this.fieldValidators, this.getFormState, setFormField
+        );
 
-          if (field.validate) {
-            const subject = new Subject();
-            const stream = field.validate(subject, this.getFormState).subscribe(
-              value => {
-                // console.log('validation', value);
-                this.props.setFormField({
-                  field: Map(cleanValidationOutput(value)),
-                  fieldName,
-                });
-              }
-            );
-            output = { stream, subject };
-          } else {
-            output = null;
-          }
-
-          return output;
-        });
-
-        // Create change handlers
-        this.fieldChangeHandlers = mapObjectToObject(config.fields, (field, fieldName) => value => {
-          this.props.setFormField({
-            fieldName,
-            field: Map({
-              isDirty: true,
-              value: field.transform ? field.transform(value) : value,
-            }),
-          });
-
-          // Trigger other fields that need to know when this one changes
-          this.triggerFieldMap[fieldName].forEach(fieldNameToTrigger => {
-            // Trigger the field to revalidate with its current value
-            this.fieldValidators[fieldNameToTrigger].subject.onNext(
-              this.props.formState.getIn(['fields', fieldNameToTrigger, 'value'])
-            )
-          });
-        });
-
-        if (!this.getFormState()) {
-          this.props.initializeForm({
-            form: new Form({
-              fields: Map(mapObjectToObject(config.fields, field => new Field({
-                value: field.initialValues ? field.initialValues.value : undefined,
-              }))),
-            }),
-            formName: config.name,
-          });
+        // Only create an empty for state if one doesn't already exist. State can persist after a
+        // form unmounts if the reduxPersistenceWrapper is being used.
+        if (!formState) {
+          initializeForm({ form: createEmptyForm(fields), formName: name });
         }
       }
 
       componentWillReceiveProps(nextProps) {
-        const { formState } = this.props;
-        nextProps.formState.fields.forEach((field, fieldName) => {
-          const isDifferent = !formState || field !== formState.fields.get(fieldName);
-          // Trigger validation stream
-          const { subject } = this.fieldValidators[fieldName];
-          if(isDifferent && subject) {
-            subject.onNext(field.value);
-          }
-        });
+        getFieldsWithDiff(this.props.formState, nextProps.formState)
+        .forEach((field, fieldName) => this.fieldValidators[fieldName].subject.onNext(field.value));
       }
 
       componentWillUnmount() {
@@ -134,14 +164,10 @@ export function formalize(config, mapFormToProps) {
       render() {
         const { formState } = this.props;
 
-        const proxyProps = mapFormToProps
-          ? mapFormToProps(formState)
-          : { form: formState };
-
         // The first render happens before initialization has time to complete so only render
         // contents afterwards to avoid lots of conditional checking for state in sub components
         return formState
-          ? <ComponentToWrap {...proxyProps} />
+          ? <ComponentToWrap {...mapFormToProps(formState)} />
           : null;
       }
     }
