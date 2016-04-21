9ec1721c5528f178390622ccf84e685327286a8e
Jamie Copeland <jamie@jamiecopeland.com>
2016-04-21 14:54:04 +0100

    Fix referencing error

--- src/components/elements/Visualizer.js
+++ src/components/elements/Visualizer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import connectForm from '../connectForm';
+import connectForm from '../connectors/connectForm';
 import { VALID, INVALID, PENDING } from '../../constants/validationStates';
 
 const tableStyle = {
